aef0487d5e34a85890a05184edb00fad42144253
Jalal_Afkhami <afkham1380@gmail.com>
2024-07-31 17:43:08 +0330

    Add Post Models

---
+++ Home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    titale = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.titile + '|' + self.author
